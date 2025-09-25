8929325b4b01b4caa6f26e007abe27214a2eb14a
Devan Desai <devdes338@gmail.com>
2025-09-24 22:19:59 -0400

    set up backend and connected to sakila database

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import { createConnection } from 'mysql2/promise';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json);
+
+try {
+  // create the connection to database
+  const connection = await createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+
+    console.log();
+    await connection.end();
+    
+} catch (err) {
+  console.log(err);
+}
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/index.html"));
